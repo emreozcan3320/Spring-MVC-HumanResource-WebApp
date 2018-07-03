f529892879991a51066feb5ac1efac9053f63297
emreozcan3320 <emreozcan3320@gmail.com>
2018-07-03 11:40:15 +0300

    adayService were added

--- insanKaynaklari/src/main/java/com/kadiremreozcan/service/AdaysService.java
+++ insanKaynaklari/src/main/java/com/kadiremreozcan/service/AdaysService.java
@@ -1,5 +1,31 @@
 package com.kadiremreozcan.service;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kadiremreozcan.dao.AdaysDAO;
+import com.kadiremreozcan.entity.Adays;
+
+@Service
+@Transactional
 public class AdaysService {
+	
+	@Autowired
+	private AdaysDAO adayDAO;
+	
+	// CREATE
+	public Long createIlan(Adays aday, HttpServletRequest req) {
+		// TODO:uzman id deðiþecek
+		return adayDAO.insert(aday);
+	}
+	
+	// READ bir adayýn bilgilerini dönüyor
+	public Adays getAdayById(Long aday_id) {
+
+			return adayDAO.getFindById(aday_id);
+		}
 
 }
