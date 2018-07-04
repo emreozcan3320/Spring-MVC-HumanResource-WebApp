4c2cc1bd5f20e9aa2e3204708910ee4ac8a66c89
emreozcan3320 <emreozcan3320@gmail.com>
2018-07-04 20:19:38 +0300

    HomeControllers configuration were checked

---
+++ insanKaynaklari/src/main/java/com/kadiremreozcan/insanKaynaklari/AdayController.java
@@ -0,0 +1,48 @@
+package com.kadiremreozcan.insanKaynaklari;
+
+import java.beans.PropertyVetoException;
+
+import org.hibernate.HibernateException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AdayController {
+	
+	/*
+	 * ADAY ENDPOINTLERÝ BAÞLANGICI
+	 * 
+	 */
+
+	@RequestMapping(value = "/aday/login", method = RequestMethod.GET)
+	public String adayLogin(Model model) throws HibernateException, PropertyVetoException {
+
+		System.out.println("/aday/login");
+
+		return "adayLogin";
+	}
+
+	@RequestMapping(value = "/aday/index", method = RequestMethod.GET)
+	public String aday(Model model) throws HibernateException, PropertyVetoException {
+
+		System.out.println("/aday/index");
+
+		return "aday";
+	}
+
+	@RequestMapping(value = "/aday/basvurularim", method = RequestMethod.GET)
+	public String adayBasvurular(Model model) throws HibernateException, PropertyVetoException {
+
+		System.out.println("/aday/basvurular");
+
+		return "basvurular";
+	}
+
+	/*
+	 * ADAY ENDPOINTLERÝ BÝTÝÞÝ
+	 * 
+	 */
+
+}
